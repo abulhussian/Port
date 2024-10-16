45db6269fe2f2d594bc14f2b0749a1999c8b155b
abulhussian <abulkhanabulkhan106@gmail.com>
2024-10-16 14:08:40 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: "600px",
+      md: "1000px",
+      lg: "1020px",
+      xl: "1440px",
+      xsm: "552px",
+      mobile: "410px",
+    },
+    extend: {
+      colors:{
+        black:'#151515',
+        white:'#FFFFFF',
+        green:'#4EE1A0',
+        grey:'#D9D9D9',
+        dgrey:'#242424'
+
+      }
+    },
+  },
+  plugins: [],
+}
